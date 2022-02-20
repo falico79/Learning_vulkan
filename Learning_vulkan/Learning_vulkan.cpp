cae7857fddbf8c01dd0573c6767d8ba0fdaa47d9
Lasse Siniluoto <c6sila00@students.oamk.fi>
2022-02-20 12:22:00 +0200

    small fixes

--- Learning_vulkan/Learning_vulkan.cpp
+++ Learning_vulkan/Learning_vulkan.cpp
@@ -20,7 +20,7 @@ int main()
 	uint32_t extensionCount = 0;
 	vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
 
-	std::cout << extensionCount << "extensions supported\n";
+	std::cout << extensionCount << " extensions supported\n";
 
 	glm::mat4 matrix;
 	glm::vec4 vec;
